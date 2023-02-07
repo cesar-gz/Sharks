5bbdc19361075fa890bb0ad273f278ea8f2c846d
cesar-gz <cesarg7@csu.fullerton.edu>
2023-02-06 20:52:31 -0800

    adding a little css, some linking with react rouer dom, and two links.

--- evolution/src/App.js
+++ evolution/src/App.js
@@ -1,14 +1,33 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import CreateUser from './components/CreateUser';
+import ListUser from './components/ListUser';
+import EditUser from './components/ListUser';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h2>React CRUD operations using PHP API and MySQL</h2>
 
-      </header>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">List Users</Link>
+            </li>
+            <li>
+              <Link to="user/create">Create User</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route index element={<ListUser />} />
+          <Route path="user/create" element={<CreateUser />} />
+          <Route path="user/:id/edit" element={<EditUser />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
