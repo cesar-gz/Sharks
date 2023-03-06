3bdde6b6800a1eb385e75bb65198b5fb1680fc11
cesar-gz <cesarg7@csu.fullerton.edu>
2023-03-05 18:53:50 -0800

    finished adding CRUD operations.

---
+++ README.md
@@ -0,0 +1,6 @@
+## React CRUD operations using PHP API and MySQL
+
+I used Apache for the Web Server, AWS RDS for Database Server.
+
+This is a project I'm turning in for my Earth History Lab Final. On the surface its just as website
+the history and evolution of Sharks. I added CRUD to it so I can have fun and learn with what was supposed to be just a website I was supposed to create and present.
--- evolution/src/components/CreateShark.js
+++ evolution/src/components/CreateShark.js
@@ -1,7 +1,76 @@
-// this is the page for the "Anatomy of a Shark"
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
+// this is the page for the "Anatomy of a Shark"
 export default function CreateShark() {
+
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({})
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }));
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios.post('http://localhost/api/user/save', inputs).then(function (response) {
+      console.log(response.data);
+      navigate('/');
+
+    });
+
+  }
+
   return (
-    <h2>Anatomy of a Shark</h2>
+    <div>
+      <h2>Anatomy of Sharks</h2>
+      <form onSubmit={handleSubmit}>
+        <table cellSpacing="10">
+          <tbody>
+            <tr>
+              <th>
+                <label>Name: </label>
+              </th>
+              <td>
+                <input type="text" name="name" onChange={handleChange} />
+              </td>
+            </tr>
+
+            <tr>
+              <th>
+                <label>Email: </label>
+              </th>
+              <td>
+                <input type="text" name="email" onChange={handleChange} />
+              </td>
+            </tr>
+
+            <tr>
+              <th>
+                <label>Mobile: </label>
+              </th>
+              <td>
+                <input type="text" name="mobile" onChange={handleChange} />
+              </td>
+            </tr>
+
+            <tr>
+              <td colSpan="2" align="right">
+                <button>Save</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </form>
+
+
+    </div>
   )
 }
